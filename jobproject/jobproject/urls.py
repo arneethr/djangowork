7c942dac4ff90f4393fb8bb0dfc7b00f8a7f986e
arneethr <arneethreddy1998@gmail.com>
2024-03-05 09:02:23 +0530

    applevel urls and first view

--- jobproject/jobproject/urls.py
+++ jobproject/jobproject/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-def hello(request):
-    return HttpResponse("Hello World")
+from django.urls import path,include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',hello)
+    path('',include('jobapp.urls'))
 ]
