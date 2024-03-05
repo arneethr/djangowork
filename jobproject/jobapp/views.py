276c716da171c678a04cce304477d6cc2e4665e4
arneethr <arneethreddy1998@gmail.com>
2024-03-05 22:58:24 +0530

    advanced httpresponse

--- jobproject/jobapp/views.py
+++ jobproject/jobapp/views.py
@@ -4,9 +4,16 @@ jobtitle=['first job','Second Job']
 jobdesc=['First job Desc', 'Second job Desc']
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello world')
+    joblist= "<ul>"
+    for j in jobtitle:
+        id=(jobtitle.index(j))
+        joblist+=f"<li><a href='job/{id}'>{j}</a></li>"
+    joblist+="</ul>"
+    return HttpResponse(joblist)
+
+
 def jobs(request,id):
     if id==0:
         return redirect('/')
-    returnhtml=f"<h1>{jobtitle[id-1]}</h1><br><h1>{jobdesc[id-1]}</h1>"
+    returnhtml=f"<h1>{jobtitle[id]}</h1><br><h1>{jobdesc[id]}</h1>"
     return HttpResponse(returnhtml)
