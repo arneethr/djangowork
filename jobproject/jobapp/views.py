b1456b2f3df2830fd0049b4380c607b12b95e2a6
arneethr <arneethreddy1998@gmail.com>
2024-03-05 15:50:05 +0530

    second url

--- jobproject/jobapp/views.py
+++ jobproject/jobapp/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello world')
+def jobs(request):
+    return HttpResponse('Job Details Page')
--- jobproject/jobproject/urls.py
+++ jobproject/jobproject/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from jobapp import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('jobapp.urls'))
+    path('',include('jobapp.urls')),
+    path('job/1',views.jobs)
 ]
