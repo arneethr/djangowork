649d72fd771f13b71cbdb88e67d6a388e7508756
arneethr <arneethreddy1998@gmail.com>
2024-03-05 16:38:57 +0530

    returning html

--- jobproject/jobapp/views.py
+++ jobproject/jobapp/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+jobtitle=['first job','Second Job']
+jobdesc=['First job Desc', 'Second job Desc']
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello world')
 def jobs(request,id):
-    return HttpResponse(f'<h1>Job Details Page {id}</h1>')
+    id=int(id)-1
+    returnhtml=f"<h1>{jobtitle[id]}</h1><br><h1>{jobdesc[id]}</h1>"
+    return HttpResponse(returnhtml)
